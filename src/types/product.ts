5bfe22581f99d6c7f30ed67183c178ac3e14e7f1
MaxLiebsch <liebsch@dipmaxexport.com>
2024-04-22 10:40:22 +0200

    fix: extend product

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,62 +1,64 @@
-import { Content } from ".";
-
+import { Content } from '.';
 
 export interface Product {
-   link: string;
-   image: string;
-   name: string;
-   price: string;
-   category?: string[];
-   description?: string;
-   shop?: string;
-   nameSub?:string;
-   year?: string;
-   createdAt: string
-   updatedAt: string
-   prime: boolean
+  link: string;
+  image: string;
+  name: string;
+  vendor?: string;
+  vendorLink?: string;
+  promoPrice?: string;
+  price: string;
+  category?: string[];
+  description?: string;
+  shop?: string;
+  nameSub?: string;
+  year?: string;
+  createdAt: string;
+  updatedAt: string;
+  prime: boolean;
 }
 
 export interface CandidateProduct extends Product {
-   candidateNameSplit: string []
+  nameSegments: string[];
 }
 
 export interface SrcProductDetails {
-      nm: string
-      nmSubSplit: string[]
-      prc: number
-      dscrptnSplit: string[]
-      mnfctr: string 
+  nm: string;
+  nmSubSegments: string[];
+  prc: number;
+  dscrptnSegments: string[];
+  mnfctr: string;
 }
 
-
-export type ProductRecord = Record<Content, string | boolean | Array<string>>
-
+export type ProductRecord = Record<Content, string | boolean | Array<string>>;
 
 export interface DbProduct {
-   s: string
-   ean: string
-   pblsh: boolean;
-   vrfd: boolean
-   ctgry: string[]
-   mnfctr: string
-   nm: string
-   img: string
-   lnk: string
-   prc: number
-   createdAt: string
-   updatedAt: string
-   e_lnk: string
-   e_img: string
-   e_nm: string
-   e_prc: number
-   e_mrgn: number
-   e_fat: boolean
-   e_mrgn_pct: number
-   a_lnk: string
-   a_img: string
-   a_nm: string
-   a_prc: number
-   a_mrgn: number
-   a_fat: boolean
-   a_mrgn_pct: number
- }
+  s: string;
+  ean: string;
+  anr: string; //article number
+  pblsh: boolean;
+  vrfd: boolean;
+  ctgry: string[];
+  mnfctr: string;
+  nm: string;
+  img: string;
+  lnk: string;
+  prc: number;
+  createdAt: string;
+  updatedAt: string;
+  e_lnk: string;
+  e_img: string;
+  e_nm: string;
+  e_prc: number;
+  e_mrgn: number;
+  e_fat: boolean;
+  e_mrgn_pct: number;
+  a_lnk: string;
+  a_bsr: string;
+  a_img: string;
+  a_nm: string;
+  a_prc: number;
+  a_mrgn: number;
+  a_fat: boolean;
+  a_mrgn_pct: number;
+}
