a63b1cdd6beba80740cfa45e4f9444234b3ca29c
MaxLiebsch <liebsch@dipmaxexport.com>
2024-05-07 16:34:57 +0200

    Update queue.ts

--- src/util/queue.ts
+++ src/util/queue.ts
@@ -44,6 +44,7 @@ export class CrawlerQueue {
       'Navigating frame was detached': { count: 0, lastOccurred: null },
       'Requesting main frame too early!': { count: 0, lastOccurred: null },
       'net::ERR_TIMED_OUT': { count: 0, lastOccurred: null },
+      'net::ERR_TUNNEL_CONNECTION_FAILED':{ count: 0, lastOccurred: null }
     };
     this.queueTask = task;
     this.concurrency = concurrency + 1; //new page
