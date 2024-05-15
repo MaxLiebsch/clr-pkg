487dd36ad55aed646468f39b5b8425c9d0a93053
MaxLiebsch <liebsch@dipmaxexport.com>
2024-05-15 13:59:36 +0200

    fix: some fingerprint issues pattern

--- src/util/browser/getPage.ts
+++ src/util/browser/getPage.ts
@@ -197,6 +197,73 @@ const setPageProperties = async (
     languages,
     language,
   );
+  await page.evaluateOnNewDocument(() => {
+    const originalQuery = window.navigator.permissions.query;
+    //@ts-ignore
+    return (window.navigator.permissions.query = (parameters) => {
+      return parameters.name === 'notifications'
+        ? Promise.resolve({ state: Notification.permission })
+        : originalQuery(parameters);
+    });
+  });
+  await page.evaluateOnNewDocument(() => {
+    // We can mock this in as much depth as we need for the test.
+    //@ts-ignore
+    window.navigator.chrome = {
+      app: {
+        isInstalled: false,
+      },
+      webstore: {
+        onInstallStageChanged: {},
+        onDownloadProgress: {},
+      },
+      runtime: {
+        PlatformOs: {
+          MAC: 'mac',
+          WIN: 'win',
+          ANDROID: 'android',
+          CROS: 'cros',
+          LINUX: 'linux',
+          OPENBSD: 'openbsd',
+        },
+        PlatformArch: {
+          ARM: 'arm',
+          X86_32: 'x86-32',
+          X86_64: 'x86-64',
+        },
+        PlatformNaclArch: {
+          ARM: 'arm',
+          X86_32: 'x86-32',
+          X86_64: 'x86-64',
+        },
+        RequestUpdateCheckStatus: {
+          THROTTLED: 'throttled',
+          NO_UPDATE: 'no_update',
+          UPDATE_AVAILABLE: 'update_available',
+        },
+        OnInstalledReason: {
+          INSTALL: 'install',
+          UPDATE: 'update',
+          CHROME_UPDATE: 'chrome_update',
+          SHARED_MODULE_UPDATE: 'shared_module_update',
+        },
+        OnRestartRequiredReason: {
+          APP_UPDATE: 'app_update',
+          OS_UPDATE: 'os_update',
+          PERIODIC: 'periodic',
+        },
+      },
+    };
+    
+  });
+  await page.evaluateOnNewDocument(() => {
+    // Overwrite the `plugins` property to use a custom getter.
+    Object.defineProperty(navigator, 'plugins', {
+      // This just needs to have `length > 0` for the current test,
+      // but we could mock the plugins too if necessary.
+      get: () => [1, 2, 3, 4, 5],
+    });
+  });
 };
 
 export async function getPage(
