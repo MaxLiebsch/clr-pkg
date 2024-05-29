0bb08c6a001d6e90db9ec3919d3efb67f976a682
MaxLiebsch <liebsch@dipmaxexport.com>
2024-05-29 13:41:25 +0200

    fix: yara yara

--- src/util/browser/closePage.ts
+++ src/util/browser/closePage.ts
@@ -3,7 +3,7 @@ import { Page } from 'puppeteer1'
 export const closePage = async (page: Page) => {
   
   try {
-    // !page.isClosed() && (await page.close());
+    !page.isClosed() && (await page.close());
   } catch (error) {
     if (error instanceof Error) console.log('closePage:\n', error.message);
   }
