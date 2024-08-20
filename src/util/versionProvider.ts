c83a72078282cc90959d3f1c6d19d200216975fe
MaxLiebsch <liebsch@dipmaxexport.com>
2024-08-20 18:12:37 +0200

    fix: upgrade browser

--- src/util/versionProvider.ts
+++ src/util/versionProvider.ts
@@ -1,27 +1,27 @@
-import puppeteer8 from 'puppeteer8'; // Puppeteer v22.12.0
-import puppeteer7 from 'puppeteer7'; // Puppeteer v22.11.1
-import puppeteer6 from 'puppeteer6'; // Puppeteer v22.11.0
-import puppeteer5 from 'puppeteer5'; // Puppeteer v22.10.0
-import puppeteer4 from 'puppeteer4'; // Puppeteer v22.8.2
-import puppeteer3 from 'puppeteer3'; // Puppeteer v22.9.0
-import puppeteer2 from 'puppeteer2'; // Puppeteer v22.8.0
-import puppeteer from 'puppeteer1'; // Puppeteer v22.7.0
+import puppeteer8 from 'puppeteer8'; // Puppeteer v23.1.0
+import puppeteer7 from 'puppeteer7'; // Puppeteer v23.0.2
+import puppeteer6 from 'puppeteer6'; // Puppeteer v23.0.0
+import puppeteer5 from 'puppeteer5'; // Puppeteer v22.14.0
+import puppeteer4 from 'puppeteer4'; // Puppeteer v22.13.1
+import puppeteer3 from 'puppeteer3'; // Puppeteer v22.12.1
+import puppeteer2 from 'puppeteer2'; // Puppeteer v22.12.0
+import puppeteer from 'puppeteer1'; // Puppeteer v22.11.1
 
 export const puppeteerVersions = {
-  '126.0.6478.63': puppeteer8, // Puppeteer v22.12.0
-  '126.0.6478.61': puppeteer7, // Puppeteer v22.11.1
-  '126.0.6478.55': puppeteer6, // Puppeteer v22.11.0
-  '125.0.6422.78': puppeteer5, // Puppeteer v22.10.0
-  '125.0.6422.60': puppeteer4, // Puppeteer v22.9.0
-  '124.0.6367.207': puppeteer3, // Puppeteer v22.8.2
-  '124.0.6367.91': puppeteer2, // Puppeteer v22.8.0
-  '124.0.6367.60': puppeteer, // Puppeteer v22.7.0
+  '127.0.6533.119': puppeteer8,// Puppeteer v23.1.0
+  '127.0.6533.99': puppeteer7,// Puppeteer v23.0.2
+  '127.0.6533.88': puppeteer6,// Puppeteer v23.0.0
+  '127.0.6533.72': puppeteer5,// Puppeteer v22.14.0
+  '126.0.6478.182': puppeteer4,// Puppeteer v22.13.1
+  '126.0.6478.126': puppeteer3,// Puppeteer v22.12.1
+  '126.0.6478.63': puppeteer2,// Puppeteer v22.12.0
+  '126.0.6478.61': puppeteer,// Puppeteer v22.11.1
 };
 
 export type Versions = keyof typeof puppeteerVersions;
 
 export class VersionProvider {
-  public currentPuppeteerVersion: Versions = '126.0.6478.63';
+  public currentPuppeteerVersion: Versions = '127.0.6533.119';
   public puppeteerInstance: any = puppeteer;
   public puppeteer = puppeteerVersions;
 
