72da2ca604059b571357d59748dbc2dab7593732
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-08 11:55:47 +0200

    fix: update browser versions

--- src/__test__/prefix-link.test.ts
+++ src/__test__/prefix-link.test.ts
@@ -1,10 +1,10 @@
-import { prefixLink } from "../util/matching/compare_helper";
+import { prefixLink } from '../util/matching/compare_helper';
 import { describe, expect, test } from '@jest/globals';
 
 describe('prefixLink.ts', () => {
-    test('clean up html', () => {
-        expect(
-          prefixLink("https://amazon.de/BLANCO-herausziehbarem-Brausestrahl-Wasserqualit%C3%A4t-Wassersparend/dp/B07B6LMM2Q/ref=sr_1_1?dib=eyJ2IjoiMSJ9.yfwxmNd7COLMynkVQcLVNIW-3PQ-OShzOkOfJCB3TTKcGyVvmx2jtmNk6bKWQJs1FapW2BZYNbGczRCjKbEsWyExf6OyaIRiTukU6_7CDlOF5Oy8FgCQFoIqGz3akhcgMR2zzXWDiUH7-Pul1_0JOTeTWdkXAZJ1VKEAtGhpt1GXvuBUwRmr2nZ56isV-T7FBXgS5P5EBUd_cIIk269B-XVuMFGUyn6EcHAaarCj31EfZigGBSjifPuQ1wwZiXYxQB7YKyk5SH7lq-MKj90yr-xN7smrdeZ36D2E0zA0hlY.ICWCSgLVUFLlDjjZHSn8ONFWDk4J3xvOqzcj1e3Jiaw&dib_tag=se&keywords=Blanco+Ambis-S+523119&qid=1710597890&sr=8-1", "amazon.de"),
-        ).toBe("https://www.amazon.de/BLANCO-herausziehbarem-Brausestrahl-Wasserqualit%C3%A4t-Wassersparend/dp/B07B6LMM2Q/ref=sr_1_1?dib=eyJ2IjoiMSJ9.yfwxmNd7COLMynkVQcLVNIW-3PQ-OShzOkOfJCB3TTKcGyVvmx2jtmNk6bKWQJs1FapW2BZYNbGczRCjKbEsWyExf6OyaIRiTukU6_7CDlOF5Oy8FgCQFoIqGz3akhcgMR2zzXWDiUH7-Pul1_0JOTeTWdkXAZJ1VKEAtGhpt1GXvuBUwRmr2nZ56isV-T7FBXgS5P5EBUd_cIIk269B-XVuMFGUyn6EcHAaarCj31EfZigGBSjifPuQ1wwZiXYxQB7YKyk5SH7lq-MKj90yr-xN7smrdeZ36D2E0zA0hlY.ICWCSgLVUFLlDjjZHSn8ONFWDk4J3xvOqzcj1e3Jiaw&dib_tag=se&keywords=Blanco+Ambis-S+523119&qid=1710597890&sr=8-1");
-      });
+  test('prefixlink', () => {
+    expect(
+      prefixLink('/Images/300x251/2884145_31ea132340a3.png', 'proshop.de'),
+    ).toBe('https://www.proshop.de/Images/300x251/2884145_31ea132340a3.png');
   });
+});
--- src/util/versionProvider.ts
+++ src/util/versionProvider.ts
@@ -1,27 +1,27 @@
-import puppeteer8 from 'puppeteer8'; // Puppeteer v23.3.0
-import puppeteer7 from 'puppeteer7'; // Puppeteer v23.2.1
-import puppeteer6 from 'puppeteer6'; // Puppeteer v23.2.0
-import puppeteer5 from 'puppeteer5'; // Puppeteer v23.1.1
-import puppeteer4 from 'puppeteer4'; // Puppeteer v23.0.2
-import puppeteer3 from 'puppeteer3'; // Puppeteer v23.0.0
-import puppeteer2 from 'puppeteer2'; // Puppeteer v22.14.0
-import puppeteer from 'puppeteer1'; // Puppeteer v22.13.1
+import puppeteer8 from 'puppeteer8'; // Puppeteer v23.5.0
+import puppeteer7 from 'puppeteer7'; // Puppeteer v23.4.1
+import puppeteer6 from 'puppeteer6'; // Puppeteer v23.4.0
+import puppeteer5 from 'puppeteer5'; // Puppeteer v23.3.1
+import puppeteer4 from 'puppeteer4'; // Puppeteer v23.3.0
+import puppeteer3 from 'puppeteer3'; // Puppeteer v23.2.1
+import puppeteer2 from 'puppeteer2'; // Puppeteer v23.2.0
+import puppeteer from 'puppeteer1'; // Puppeteer v23.1.1
 
 export const puppeteerVersions = {
-  '128.0.6613.119': puppeteer8,// Puppeteer v23.3.0
-  '128.0.6613.86': puppeteer7,// Puppeteer v23.2.1
-  '128.0.6613.84': puppeteer6,// Puppeteer v23.2.0
-  '127.0.6533.119': puppeteer5,// Puppeteer v23.1.1
-  '127.0.6533.99': puppeteer4,// Puppeteer v23.0.2
-  '127.0.6533.88': puppeteer3,// Puppeteer v23.0.0
-  '127.0.6533.72': puppeteer2,// Puppeteer v22.14.0
-  '126.0.6478.182': puppeteer,// Puppeteer v22.13.1
+  '129.0.6668.89': puppeteer8,// Puppeteer v23.5.0
+  '129.0.6668.70': puppeteer7,// Puppeteer v23.4.1
+  '129.0.6668.58': puppeteer6,// Puppeteer v23.4.0
+  '128.0.6613.137	': puppeteer5,// Puppeteer v23.3.1
+  '128.0.6613.119	': puppeteer4,// Puppeteer v23.3.0
+  '128.0.6613.86': puppeteer3,// Puppeteer v23.2.1
+  '128.0.6613.84': puppeteer2,// Puppeteer v23.2.0
+  '127.0.6533.119': puppeteer,// Puppeteer v23.1.1
 };
 
 export type Versions = keyof typeof puppeteerVersions;
 
 export class VersionProvider {
-  public currentPuppeteerVersion: Versions = '128.0.6613.119';
+  public currentPuppeteerVersion: Versions = '129.0.6668.89';
   public puppeteerInstance: any = puppeteer;
   public puppeteer = puppeteerVersions;
 
