b364f139a30625240244a84f20c658de6d15927f
MaxLiebsch <liebsch@dipmaxexport.com>
2024-10-16 13:38:42 +0200

    Update closePage.ts

--- src/util/browser/closePage.ts
+++ src/util/browser/closePage.ts
@@ -3,7 +3,7 @@ import { Page } from 'puppeteer1'
 export const closePage = async (page: Page) => {
   
   try {
-    // !page.isClosed() && page && (await page.close());
+    !page.isClosed() && page && (await page.close());
   } catch (error) {
     if (error instanceof Error) console.log('closePage:\n', error.message);
   }
